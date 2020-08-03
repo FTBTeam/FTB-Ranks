9ffa6d89948e306ee02f929480144e316d83b888
LatvianModder <latvianmodder@gmail.com>
2020-08-03 17:17:37 +0300

    Server gui fix

--- src/main/java/com/feed_the_beast/mods/ftbranks/FTBRanks.java
+++ src/main/java/com/feed_the_beast/mods/ftbranks/FTBRanks.java
@@ -25,9 +25,13 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.server.permission.PermissionAPI;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -46,6 +50,7 @@ public class FTBRanks
 		FTBRanksAPI.INSTANCE = new FTBRanksAPIImpl();
 		MinecraftForge.EVENT_BUS.register(FTBRanks.class);
 		PermissionAPI.setPermissionHandler(new PermissionAPIWrapper(PermissionAPI.getPermissionHandler()));
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 
 	@SubscribeEvent
