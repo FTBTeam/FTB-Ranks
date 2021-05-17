d11b2a974235282d7e5990f9991b7ff3569dcbb6
LatvianModder <latvianmodder@gmail.com>
2021-05-18 00:17:30 +0300

    Added fabric CF ID

--- forge/src/main/java/dev/ftb/mods/ftbranks/forge/FTBRanksForge.java
+++ forge/src/main/java/dev/ftb/mods/ftbranks/forge/FTBRanksForge.java
@@ -1,9 +1,11 @@
 package dev.ftb.mods.ftbranks.forge;
 
 import dev.ftb.mods.ftbranks.FTBRanks;
+import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.server.permission.PermissionAPI;
 import org.apache.commons.lang3.tuple.Pair;
@@ -11,6 +13,7 @@ import org.apache.commons.lang3.tuple.Pair;
 @Mod(FTBRanks.MOD_ID)
 public class FTBRanksForge {
 	public FTBRanksForge() {
+		EventBuses.registerModEventBus(FTBRanks.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 		new FTBRanks();
 		PermissionAPI.setPermissionHandler(new PermissionAPIWrapper(PermissionAPI.getPermissionHandler()));
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
