06ccffbf6933264818f7d3dd307a9f4c8be94a62
X_Niter <drumsforlife1999@yahoo.com>
2022-05-21 20:13:59 -0500

    1.18.2/QuarkTagging/Worky/Yay/YouWantIt? Get it your self #Read/ :) #lephro :)

--- common/src/main/java/dev/ftb/mods/ftbranks/impl/FTBRanksAPIImpl.java
+++ common/src/main/java/dev/ftb/mods/ftbranks/impl/FTBRanksAPIImpl.java
@@ -1,7 +1,10 @@
 package dev.ftb.mods.ftbranks.impl;
 
+import com.mojang.brigadier.Message;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.ChatEvent;
+import dev.architectury.event.events.common.LifecycleEvent;
+import dev.architectury.event.events.common.TickEvent;
 import dev.ftb.mods.ftblibrary.util.TextComponentUtils;
 import dev.ftb.mods.ftbranks.FTBRanks;
 import dev.ftb.mods.ftbranks.api.FTBRanksAPI;
@@ -20,13 +23,17 @@ import dev.ftb.mods.ftbranks.impl.condition.SpawnCondition;
 import dev.ftb.mods.ftbranks.impl.condition.StatCondition;
 import dev.ftb.mods.ftbranks.impl.condition.XorCondition;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.HoverEvent;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.*;
+import net.minecraft.network.protocol.game.ServerboundChatPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.TextFilter;
+import net.minecraft.world.InteractionResultHolder;
+
+import java.text.CompactNumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LatvianModder
@@ -111,43 +118,55 @@ public class FTBRanksAPIImpl extends FTBRanksAPI {
 
 		main.append(cachedNameForChat);
 		main.append(" ");
-		//main.appendText("<").appendSibling(event.getPlayer().getDisplayName()).appendText(">").appendText(" ");
 
 		String message = eventMessage.getFiltered().trim();
 
-		Component textWithLinks = TextComponentUtils.withLinks(message);
-		TextComponent text = textWithLinks instanceof TextComponent ? (TextComponent) textWithLinks : new TextComponent(message);
+
 
 		ChatFormatting color = ChatFormatting.getByName(FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.color").asString().orElse(null));
 
 		if (color != null) {
-			text.setStyle(text.getStyle().applyFormat(color));
+			main.setStyle(main.getStyle().applyFormat(color));
 		}
 
 		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.bold").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.BOLD));
+			main.setStyle(main.getStyle().applyFormat(ChatFormatting.BOLD));
 		}
 
 		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.italic").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.ITALIC));
+			main.setStyle(main.getStyle().applyFormat(ChatFormatting.ITALIC));
 		}
 
 		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.underlined").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.UNDERLINE));
+			main.setStyle(main.getStyle().applyFormat(ChatFormatting.UNDERLINE));
 		}
 
 		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.strikethrough").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.STRIKETHROUGH));
+			main.setStyle(main.getStyle().applyFormat(ChatFormatting.STRIKETHROUGH));
 		}
 
 		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.obfuscated").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.OBFUSCATED));
+			main.setStyle(main.getStyle().applyFormat(ChatFormatting.OBFUSCATED));
 		}
 
-		main.append(text);
+
+		//component.getFiltered().getSiblings().size();
+		component.getFiltered().getSiblings();
+
+		TranslatableComponent fullComp = (TranslatableComponent) component.getFiltered();
+
+
+		for(int i=1; i < fullComp.getArgs().length; i++){
+			//TextComponent part = (TextComponent) trans.getArgs()[i];
+
+			TextComponent part = fullComp.getArgs()[i] instanceof Component ? (TextComponent) fullComp.getArgs()[i] : new TextComponent((String) fullComp.getArgs()[i]);
+
+
+			main.append(part);
+		}
+		//main.append(trans);
 
 		component.setFiltered(main);
-
 		return EventResult.interruptTrue();
 	}
 }
