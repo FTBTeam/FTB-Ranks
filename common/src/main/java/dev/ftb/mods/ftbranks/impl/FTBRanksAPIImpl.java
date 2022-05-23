90d97aa630460801e425fda95dc6e61a5c751a8b
Riley Brown <30506033+x-niter@users.noreply.github.com>
2022-05-23 19:57:45 +0200

    Introduce changes fron #27 to 1.18

--- common/src/main/java/dev/ftb/mods/ftbranks/impl/FTBRanksAPIImpl.java
+++ common/src/main/java/dev/ftb/mods/ftbranks/impl/FTBRanksAPIImpl.java
@@ -1,10 +1,7 @@
 package dev.ftb.mods.ftbranks.impl;
 
-import com.mojang.brigadier.Message;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.ChatEvent;
-import dev.architectury.event.events.common.LifecycleEvent;
-import dev.architectury.event.events.common.TickEvent;
 import dev.ftb.mods.ftblibrary.util.TextComponentUtils;
 import dev.ftb.mods.ftbranks.FTBRanks;
 import dev.ftb.mods.ftbranks.api.FTBRanksAPI;
@@ -23,17 +20,15 @@ import dev.ftb.mods.ftbranks.impl.condition.SpawnCondition;
 import dev.ftb.mods.ftbranks.impl.condition.StatCondition;
 import dev.ftb.mods.ftbranks.impl.condition.XorCondition;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.*;
-import net.minecraft.network.protocol.game.ServerboundChatPacket;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.TextFilter;
-import net.minecraft.world.InteractionResultHolder;
-
-import java.text.CompactNumberFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author LatvianModder
@@ -119,34 +114,52 @@ public class FTBRanksAPIImpl extends FTBRanksAPI {
 		main.append(cachedNameForChat);
 		main.append(" ");
 
-		ChatFormatting color = ChatFormatting.getByName(FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.color").asString().orElse(null));
-		TranslatableComponent fullComp = (TranslatableComponent) component.getFiltered();
+		MutableComponent text = null;
 
-		for(int i=1; i < fullComp.getArgs().length; i++){
-
-			TextComponent part = fullComp.getArgs()[i] instanceof Component ? (TextComponent) fullComp.getArgs()[i] : new TextComponent((String) fullComp.getArgs()[i]);
-
-			if (color != null) {
-				part.setStyle(part.getStyle().applyFormat(color));
+		// In the easiest case, we have the vanilla chat format,
+		// so we can just use the message from that.
+		if (component instanceof TranslatableComponent tc && tc.getKey().equals("chat.type.text") && tc.getArgs().length > 1) {
+			Object message = tc.getArgs()[1];
+			if (message instanceof Component c) {
+				text = c.copy();
+			} else {
+				text = new TextComponent(message.toString());
 			}
-			if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.bold").asBooleanOrFalse()) {
-				part.setStyle(part.getStyle().applyFormat(ChatFormatting.BOLD));
-			}
-			if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.italic").asBooleanOrFalse()) {
-				part.setStyle(part.getStyle().applyFormat(ChatFormatting.ITALIC));
-			}
-			if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.underlined").asBooleanOrFalse()) {
-				part.setStyle(part.getStyle().applyFormat(ChatFormatting.UNDERLINE));
-			}
-			if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.strikethrough").asBooleanOrFalse()) {
-				part.setStyle(part.getStyle().applyFormat(ChatFormatting.STRIKETHROUGH));
-			}
-			if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.obfuscated").asBooleanOrFalse()) {
-				part.setStyle(part.getStyle().applyFormat(ChatFormatting.OBFUSCATED));
-			}
-
-			main.append(part);
 		}
+
+		// Otherwise, fall back to parsing the message as a string and turning it back into a component.
+		if (text == null) {
+			FTBRanks.LOGGER.debug("Chat message format has been changed, fall back to parsing as string!");
+			FTBRanks.LOGGER.debug("Since this may break formatting, feel free to remove the `ftbranks.name_format` permission node to stop this from happening.");
+			text = TextComponentUtils.withLinks(eventMessage.getFiltered()).copy();
+		}
+
+		ChatFormatting color = ChatFormatting.getByName(FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.color").asString().orElse(null));
+		if (color != null) {
+			text.setStyle(text.getStyle().applyFormat(color));
+		}
+
+		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.bold").asBooleanOrFalse()) {
+			text.setStyle(text.getStyle().applyFormat(ChatFormatting.BOLD));
+		}
+
+		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.italic").asBooleanOrFalse()) {
+			text.setStyle(text.getStyle().applyFormat(ChatFormatting.ITALIC));
+		}
+
+		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.underlined").asBooleanOrFalse()) {
+			text.setStyle(text.getStyle().applyFormat(ChatFormatting.UNDERLINE));
+		}
+
+		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.strikethrough").asBooleanOrFalse()) {
+			text.setStyle(text.getStyle().applyFormat(ChatFormatting.STRIKETHROUGH));
+		}
+
+		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.obfuscated").asBooleanOrFalse()) {
+			text.setStyle(text.getStyle().applyFormat(ChatFormatting.OBFUSCATED));
+		}
+
+		main.append(text);
 
 		component.setFiltered(main);
 		return EventResult.interruptTrue();
