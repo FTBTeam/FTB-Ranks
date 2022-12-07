7752cc9bc549ab1b07f5a93d49a29caf9a491b84
Des Herriott <des.herriott@gmail.com>
2022-12-07 09:36:26 +0000

    fix: make quark item linking work again
    Something got very strange with PR's 27/28/29/30; the changes
    from 28 didn't survive the last merge, and the changes in 27 never even
    made it into the previous release (and don't appear to be necessary in
    any case).
    
    Re-implemented 28 in a safer fashion, getting rid of the unchecked cast.
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/567

--- common/src/main/java/dev/ftb/mods/ftbranks/impl/FTBRanksAPIImpl.java
+++ common/src/main/java/dev/ftb/mods/ftbranks/impl/FTBRanksAPIImpl.java
@@ -2,27 +2,12 @@ package dev.ftb.mods.ftbranks.impl;
 
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.ChatEvent;
-import dev.ftb.mods.ftblibrary.util.TextComponentUtils;
 import dev.ftb.mods.ftbranks.FTBRanks;
 import dev.ftb.mods.ftbranks.api.FTBRanksAPI;
 import dev.ftb.mods.ftbranks.api.RankManager;
-import dev.ftb.mods.ftbranks.impl.condition.AlwaysActiveCondition;
-import dev.ftb.mods.ftbranks.impl.condition.AndCondition;
-import dev.ftb.mods.ftbranks.impl.condition.CreativeModeCondition;
-import dev.ftb.mods.ftbranks.impl.condition.DimensionCondition;
-import dev.ftb.mods.ftbranks.impl.condition.FakePlayerCondition;
-import dev.ftb.mods.ftbranks.impl.condition.NotCondition;
-import dev.ftb.mods.ftbranks.impl.condition.OPCondition;
-import dev.ftb.mods.ftbranks.impl.condition.OrCondition;
-import dev.ftb.mods.ftbranks.impl.condition.PlaytimeCondition;
-import dev.ftb.mods.ftbranks.impl.condition.RankAddedCondition;
-import dev.ftb.mods.ftbranks.impl.condition.SpawnCondition;
-import dev.ftb.mods.ftbranks.impl.condition.StatCondition;
-import dev.ftb.mods.ftbranks.impl.condition.XorCondition;
+import dev.ftb.mods.ftbranks.impl.condition.*;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.MinecraftServer;
@@ -34,134 +19,108 @@ import net.minecraft.server.network.TextFilter;
  * @author LatvianModder
  */
 public class FTBRanksAPIImpl extends FTBRanksAPI {
-	public static RankManagerImpl manager;
+    public static RankManagerImpl manager;
 
-	@Override
-	public RankManager getManager() {
-		return manager;
-	}
+    @Override
+    public RankManager getManager() {
+        return manager;
+    }
 
-	public static void serverAboutToStart(MinecraftServer server) {
-		manager = new RankManagerImpl(server);
-	}
+    public static void serverAboutToStart(MinecraftServer server) {
+        manager = new RankManagerImpl(server);
+    }
 
-	public static void serverStarted(MinecraftServer server) {
-		// manager.initCommands();
+    public static void serverStarted(MinecraftServer server) {
+        try {
+            manager.load();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-		try {
-			manager.load();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void serverStopped(MinecraftServer server) {
+        manager = null;
+    }
 
-	public static void serverStopped(MinecraftServer server) {
-		manager = null;
-	}
+    public static void worldSaved(ServerLevel event) {
+        if (manager != null) {
+            manager.saveRanksNow();
+            manager.savePlayersNow();
+        }
+    }
 
-	public static void worldSaved(ServerLevel event) {
-		if (manager != null) {
-			manager.saveRanksNow();
-			manager.savePlayersNow();
-		}
-	}
+    public static void serverStarting(MinecraftServer server) {
+        manager.registerCondition("always_active", (rank, json) -> AlwaysActiveCondition.INSTANCE);
+        manager.registerCondition("rank_added", RankAddedCondition::new);
 
-	public static void serverStarting(MinecraftServer server) {
-		manager.registerCondition("always_active", (rank, json) -> AlwaysActiveCondition.INSTANCE);
-		manager.registerCondition("rank_added", RankAddedCondition::new);
+        manager.registerCondition("not", NotCondition::new);
+        manager.registerCondition("or", OrCondition::new);
+        manager.registerCondition("and", AndCondition::new);
+        manager.registerCondition("xor", XorCondition::new);
 
-		manager.registerCondition("not", NotCondition::new);
-		manager.registerCondition("or", OrCondition::new);
-		manager.registerCondition("and", AndCondition::new);
-		manager.registerCondition("xor", XorCondition::new);
+        manager.registerCondition("op", (rank, tag) -> new OPCondition());
+        manager.registerCondition("spawn", (rank, tag) -> new SpawnCondition());
+        manager.registerCondition("dimension", (rank, tag) -> new DimensionCondition(tag));
+        manager.registerCondition("playtime", (rank, tag) -> new PlaytimeCondition(tag));
+        manager.registerCondition("stat", (rank, tag) -> new StatCondition(tag));
+        manager.registerCondition("fake_player", (rank, tag) -> new FakePlayerCondition());
+        manager.registerCondition("creative_mode", (rank, tag) -> new CreativeModeCondition());
+    }
 
-		manager.registerCondition("op", (rank, tag) -> new OPCondition());
-		manager.registerCondition("spawn", (rank, tag) -> new SpawnCondition());
-		manager.registerCondition("dimension", (rank, tag) -> new DimensionCondition(tag));
-		manager.registerCondition("playtime", (rank, tag) -> new PlaytimeCondition(tag));
-		manager.registerCondition("stat", (rank, tag) -> new StatCondition(tag));
-		manager.registerCondition("fake_player", (rank, tag) -> new FakePlayerCondition());
-		manager.registerCondition("creative_mode", (rank, tag) -> new CreativeModeCondition());
-	}
+    public static EventResult serverChat(ServerPlayer player, TextFilter.FilteredText eventMessage, ChatEvent.ChatComponent component) {
+        String format = FTBRanksAPI.getPermissionValue(player, "ftbranks.name_format").asString().orElse("");
 
+        if (format.isEmpty()) {
+            return EventResult.pass();
+        }
 
-	public static EventResult serverChat(ServerPlayer player, TextFilter.FilteredText eventMessage, ChatEvent.ChatComponent component) {
-		String format = FTBRanksAPI.getPermissionValue(player, "ftbranks.name_format").asString().orElse("");
+        TextComponent main = new TextComponent("");
+        TextComponent cachedNameForChat;
 
-		if (format.isEmpty()) {
-			return EventResult.pass();
-		}
+        try {
+            cachedNameForChat = TextComponentParser.parse(format, s -> s.equals("name") ? player.getDisplayName() : null);
+        } catch (Exception ex) {
+            String s = "Error parsing " + format + ": " + ex;
+            FTBRanks.LOGGER.error(s);
+            cachedNameForChat = new TextComponent("BrokenFormatting");
+            cachedNameForChat.withStyle(ChatFormatting.RED);
+            cachedNameForChat.setStyle(cachedNameForChat.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent(s))));
+        }
 
-		TextComponent main = new TextComponent("");
-		TextComponent cachedNameForChat;
+        main.append(cachedNameForChat);
+        main.append(" ");
 
-		try {
-			cachedNameForChat = TextComponentParser.parse(format, s -> {
-				if (s.equals("name")) {
-					return player.getDisplayName();
-				}
+        if (component.getFiltered() instanceof TranslatableComponent fullComp) {
+            ChatFormatting color = ChatFormatting.getByName(FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.color").asString().orElse(null));
+            for (int i = 1; i < fullComp.getArgs().length; i++) {
+                Object arg = fullComp.getArgs()[i];
+                TextComponent part = arg instanceof TextComponent tc ? tc : new TextComponent(arg.toString());
 
-				return null;
-			});
-		} catch (Exception ex) {
-			String s = "Error parsing " + format + ": " + ex;
-			FTBRanks.LOGGER.error(s);
-			cachedNameForChat = new TextComponent("BrokenFormatting");
-			cachedNameForChat.withStyle(ChatFormatting.RED);
-			cachedNameForChat.setStyle(cachedNameForChat.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent(s))));
-		}
+                if (color != null) {
+                    part.setStyle(part.getStyle().applyFormat(color));
+                }
+                if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.bold").asBooleanOrFalse()) {
+                    part.setStyle(part.getStyle().applyFormat(ChatFormatting.BOLD));
+                }
+                if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.italic").asBooleanOrFalse()) {
+                    part.setStyle(part.getStyle().applyFormat(ChatFormatting.ITALIC));
+                }
+                if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.underlined").asBooleanOrFalse()) {
+                    part.setStyle(part.getStyle().applyFormat(ChatFormatting.UNDERLINE));
+                }
+                if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.strikethrough").asBooleanOrFalse()) {
+                    part.setStyle(part.getStyle().applyFormat(ChatFormatting.STRIKETHROUGH));
+                }
+                if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.obfuscated").asBooleanOrFalse()) {
+                    part.setStyle(part.getStyle().applyFormat(ChatFormatting.OBFUSCATED));
+                }
+                main.append(part);
+            }
+        } else {
+            main.append(component.getFiltered());
+        }
 
-		main.append(cachedNameForChat);
-		main.append(" ");
-
-		MutableComponent text = null;
-
-		// In the easiest case, we have the vanilla chat format,
-		// so we can just use the message from that.
-		if (component instanceof TranslatableComponent tc && tc.getKey().equals("chat.type.text") && tc.getArgs().length > 1) {
-			Object message = tc.getArgs()[1];
-			if (message instanceof Component c) {
-				text = c.copy();
-			} else {
-				text = new TextComponent(message.toString());
-			}
-		}
-
-		// Otherwise, fall back to parsing the message as a string and turning it back into a component.
-		if (text == null) {
-			FTBRanks.LOGGER.debug("Chat message format has been changed, fall back to parsing as string!");
-			FTBRanks.LOGGER.debug("Since this may break formatting, feel free to remove the `ftbranks.name_format` permission node to stop this from happening.");
-			text = TextComponentUtils.withLinks(eventMessage.getFiltered()).copy();
-		}
-
-		ChatFormatting color = ChatFormatting.getByName(FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.color").asString().orElse(null));
-		if (color != null) {
-			text.setStyle(text.getStyle().applyFormat(color));
-		}
-
-		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.bold").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.BOLD));
-		}
-
-		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.italic").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.ITALIC));
-		}
-
-		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.underlined").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.UNDERLINE));
-		}
-
-		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.strikethrough").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.STRIKETHROUGH));
-		}
-
-		if (FTBRanksAPI.getPermissionValue(player, "ftbranks.chat_text.obfuscated").asBooleanOrFalse()) {
-			text.setStyle(text.getStyle().applyFormat(ChatFormatting.OBFUSCATED));
-		}
-
-		main.append(text);
-
-		component.setFiltered(main);
-		return EventResult.interruptTrue();
-	}
+        component.setFiltered(main);
+        return EventResult.interruptTrue();
+    }
 }
