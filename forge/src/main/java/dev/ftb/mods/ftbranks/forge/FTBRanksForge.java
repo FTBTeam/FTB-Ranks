dbfd08b1b0166e00d6315a624e2795756c95731f
Des Herriott <des.herriott@gmail.com>
2022-12-12 10:05:11 +0000

    fix: do rank tab completion via server-side suggestions
    No more need to be installed on the client

--- forge/src/main/java/dev/ftb/mods/ftbranks/forge/FTBRanksForge.java
+++ forge/src/main/java/dev/ftb/mods/ftbranks/forge/FTBRanksForge.java
@@ -2,8 +2,11 @@ package dev.ftb.mods.ftbranks.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import dev.ftb.mods.ftbranks.FTBRanks;
+import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(FTBRanks.MOD_ID)
 public class FTBRanksForge {
@@ -12,6 +15,6 @@ public class FTBRanksForge {
 		new FTBRanks();
 		// Nope.
 		// PermissionAPI.setPermissionHandler(new PermissionAPIWrapper(PermissionAPI.getPermissionHandler()));
-//		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 }
